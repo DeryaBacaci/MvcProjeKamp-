9238cee15d1c504770087775d9f7b74b4a0d5f4f
DeryaBacaci <107432656+DeryaBacaci@users.noreply.github.com>
2023-02-01 17:12:06 +0300

    Görev2

---
+++ MvcProjeKampı/Controllers/İstatistikController.cs
@@ -0,0 +1,39 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace MvcProjeKampı.Controllers
+{
+    
+
+    public class İstatistikController : Controller
+    {
+        CategoryManager cm = new CategoryManager(new EfCategoryDal());
+        EfCategoryDal db= new EfCategoryDal();
+        // GET: İstatistik
+        public ActionResult Ödev2()
+        {
+            var toplamkategori = cm.GetList().Count();
+            ViewBag.ToplamKategori = toplamkategori;
+
+            var yazılımbaslıksayısı = cm.GetList().Where(x=>x.CategoryName=="Yazılım").Count();
+            ViewBag.yb = yazılımbaslıksayısı;
+
+
+            var aharfliyazar = cm.GetList().Where(x => x.CategoryName == "a").Count();
+            ViewBag.AharfliYazar=aharfliyazar;
+
+            var enfazlakategoribaslik=cm.GetList().GroupBy(x=>x.CategoryName).OrderByDescending(x=>x.Count()).Select(x=>x.Key).FirstOrDefault();
+            ViewBag.EnFazlaKategoriBaslik=enfazlakategoribaslik;
+
+            var kategoritrue=cm.GetList().Where(x=>x.CategoryStatus==true).Count();
+            ViewBag.KategoriTrue=kategoritrue;
+            return View();
+        }
+    }
+}
